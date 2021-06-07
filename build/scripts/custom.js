e4c8ff5d2f50db6ef1adec7291a24ffff1e1027f
kristiwalker <kristiwalker22@gmail.com>
2021-06-06 21:17:50 -0400

    adds conditional styling for non-url portfolio item head

--- build/scripts/custom.js
+++ build/scripts/custom.js
@@ -1,26 +1,33 @@
 /*jshint esversion: 6 */
 
 const request = new XMLHttpRequest();
-request.open('GET', 'assets/data/portfolio-items.json', true);
+request.open("GET", "assets/data/portfolio-items.json", true);
 
-request.onload = function() {
+request.onload = function () {
   if (request.status >= 200 && request.status < 400) {
     // Success!
     var portfolioItems = JSON.parse(request.responseText);
 
-    const markup = `${portfolioItems.map(item =>
-        `<li class="portfolio-item">
+    const markup = `${portfolioItems
+      .map(
+        (item) =>
+          `<li class="portfolio-item">
            <ul class="portfolio-item-type">
             <li>${item.types}</li>
            </ul>
            <p class="portfolio-item-head">
-               <a href="${item.url}" onclick="trackOutboundLink('${item.url}); return false;" target="_blank"><span>${item.head}</span></a>
+               ${
+                 item.url.length
+                   ? `<a href="${item.url}" onclick="trackOutboundLink('${item.url}); return false;" target="_blank"><span>${item.head}</span></a>`
+                   : `${item.head}`
+               }
            </p>
            <p class="portfolio-item-sub">${item.sub}</p>
        </li>`
-     ).join('')}`;
+      )
+      .join("")}`;
 
-    document.querySelector('.portfolio').innerHTML = markup;
+    document.querySelector(".portfolio").innerHTML = markup;
   }
 };
 
